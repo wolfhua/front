cb341db03b1f2c22e68fc1f2d7c86d818eaf254c
houzhonghua <18011468389@163.com>
2022-02-28 15:37:28 +0800

    测试提交

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,49 +4,113 @@
       <div class="layui-tab layui-tab-brief" lay-filter="user">
         <ul class="layui-tab-title">
           <li class="layui-this">登入</li>
-          <li><router-link :to="{name: 'Reg'}">注册</router-link></li>
+          <li><router-link :to="{ name: 'Reg' }">注册</router-link></li>
         </ul>
-        <div class="layui-form layui-tab-content" id="LAY_ucm" style="padding: 20px 0;">
+        <div
+          class="layui-form layui-tab-content"
+          id="LAY_ucm"
+          style="padding: 20px 0"
+        >
           <ValidationObserver ref="observer" v-slot="{ validate }">
             <div class="layui-tab-item layui-show">
               <div class="layui-form layui-form-pane">
                 <form method="post">
-                  <ValidationProvider class="layui-form-item" name="username" tag="div" rules="required|email" v-slot="{ errors }">
+                  <ValidationProvider
+                    class="layui-form-item"
+                    name="username"
+                    tag="div"
+                    rules="required|email"
+                    v-slot="{ errors }"
+                  >
                     <label class="layui-form-label">邮箱</label>
                     <div class="layui-input-inline">
-                      <input type="text" v-model="username" name="username" autocomplete="off" class="layui-input">
+                      <input
+                        type="text"
+                        v-model="username"
+                        name="username"
+                        autocomplete="off"
+                        class="layui-input"
+                      />
                     </div>
                     <div class="error layui-form-mid">{{ errors[0] }}</div>
                   </ValidationProvider>
-                  <ValidationProvider class="layui-form-item" name="password" tag="div" rules="required|password:6,12" v-slot="{ errors }">
+                  <ValidationProvider
+                    class="layui-form-item"
+                    name="password"
+                    tag="div"
+                    rules="required|password:6,12"
+                    v-slot="{ errors }"
+                  >
                     <label class="layui-form-label">密码</label>
                     <div class="layui-input-inline">
-                      <input type="password" v-model="password" name="password" autocomplete="off" class="layui-input">
+                      <input
+                        type="password"
+                        v-model="password"
+                        name="password"
+                        autocomplete="off"
+                        class="layui-input"
+                      />
                     </div>
                     <div class="error layui-form-mid">{{ errors[0] }}</div>
                   </ValidationProvider>
-                  <ValidationProvider class="layui-form-item" name="code" ref="codeField" tag="div" rules="required|length:4" v-slot="{ errors }">
+                  <ValidationProvider
+                    class="layui-form-item"
+                    name="code"
+                    ref="codeField"
+                    tag="div"
+                    rules="required|length:4"
+                    v-slot="{ errors }"
+                  >
                     <div class="layui-row">
                       <label class="layui-form-label">验证码</label>
                       <div class="layui-input-inline">
-                        <input type="text" v-model="code" name="code" placeholder="请输入验证码" autocomplete="off" class="layui-input">
+                        <input
+                          type="text"
+                          v-model="code"
+                          name="code"
+                          placeholder="请输入验证码"
+                          autocomplete="off"
+                          class="layui-input"
+                        />
                       </div>
                       <div>
-                        <span style="color: #c00;" v-html="svg" @click="getCode()"></span>
+                        <span
+                          style="color: #c00"
+                          v-html="svg"
+                          @click="getCode()"
+                        ></span>
                       </div>
                     </div>
                     <div class="error layui-form-mid">{{ errors[0] }}</div>
                   </ValidationProvider>
                   <div class="layui-form-item">
-                    <button class="layui-btn" type="button" @click="validate().then(submit)">立即登录</button>
-                    <span style="padding-left:20px;">
-                      <router-link :to="{name: 'Forget'}">忘记密码？</router-link>
+                    <button
+                      class="layui-btn"
+                      type="button"
+                      @click="validate().then(submit)"
+                    >
+                      立即登录
+                    </button>
+                    <span style="padding-left: 20px">
+                      <router-link :to="{ name: 'Forget' }"
+                        >忘记密码？</router-link
+                      >
                     </span>
                   </div>
                   <div class="layui-form-item fly-form-app">
-                    <span>或者使用社交账号登入</span>
-                    <a href="" onclick="layer.msg('正在通过QQ登入', {icon:16, shade: 0.1, time:0})" class="iconfont icon-qq" title="QQ登入"></a>
-                    <a href="" onclick="layer.msg('正在通过微博登入', {icon:16, shade: 0.1, time:0})" class="iconfont icon-weibo" title="微博登入"></a>
+                    <span>或者使用社交账号进行登录</span>
+                    <a
+                      href=""
+                      onclick="layer.msg('正在通过QQ登入', {icon:16, shade: 0.1, time:0})"
+                      class="iconfont icon-qq"
+                      title="QQ登入"
+                    ></a>
+                    <a
+                      href=""
+                      onclick="layer.msg('正在通过微博登入', {icon:16, shade: 0.1, time:0})"
+                      class="iconfont icon-weibo"
+                      title="微博登入"
+                    ></a>
                   </div>
                 </form>
               </div>
@@ -158,7 +222,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.error{
+.error {
   color: red;
   font-weight: 600;
 }
