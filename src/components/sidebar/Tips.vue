12c977623b8e11bb2a24ed7c991380d1197d5f01
houzhonghua <18011468389@163.com>
2022-03-08 11:31:15 +0800

    完成首页静态样式布局

---
+++ src/components/sidebar/Tips.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="fly-panel">
+    <h3 class="fly-panel-title">温馨通道</h3>
+    <div class="fly-panel-main fly-list-static">
+      <ul class="layui-clear simu-quick">
+        <li class="layui-col-xs6">
+          <a href="" target="_blank">simu-simu</a>
+        </li>
+        <li class="layui-col-xs6">
+          <a href="" target="_blank">simu-simu</a>
+        </li>
+        <li class="layui-col-xs6">
+          <a href="" target="_blank">simu-simu</a>
+        </li>
+        <li class="layui-col-xs6">
+          <a href="" target="_blank">simu-simu</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'tips'
+}
+</script>
+
+<style lang="scss" scoped>
+$border_color: #f2f2f2;
+.fly-panel-main {
+  padding: 15px;
+}
+.simu-quick {
+  text-align: center;
+  border: 1px solid $border_color;
+  border-right: none;
+  border-bottom: none;
+  li {
+    height: 40px;
+    line-height: 40px;
+    padding: 0 10px;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    border: 1px solid $border_color;
+    border-left: none;
+    border-top: none;
+    a {
+      display: block;
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,33 +1,17 @@
 <template>
   <div>
-    <div class="panel">
-      <div class="layui-container">
-        <ul>
-          <li class="layui-hide-xs layui-hide-sm layui-show-md-inline-block">
-            <a href="">首页</a>
-          </li>
-          <li><a href="">提问</a></li>
-          <li><a href="">分享</a></li>
-          <li><a href="">讨论</a></li>
-          <li><a href="">建议</a></li>
-          <li><a href="">公告</a></li>
-          <li><a href="">动态</a></li>
-          <li class="layui-hide-xs layui-hide-sm layui-show-md-inline-block">
-            <span class="line"></span>
-          </li>
-          <li class="layui-hide-xs layui-hide-sm layui-show-md-inline-block">
-            <a href="">我发布的贴</a>
-          </li>
-          <li class="layui-hide-xs layui-hide-sm layui-show-md-inline-block">
-            <a href="">我收藏的贴</a>
-          </li>
-        </ul>
-        <div class="right layui-hide-xs">
-          <span class="layui-icon layui-icon-search"></span>
-          <a href="" class="layui-btn">发布新帖</a>
+    <simu-pannel></simu-pannel>
+    <div class="layui-container">
+      <div class="layui-row layui-col-space15">
+        <div class="layui-col-md8">
+          <simu-list></simu-list>
         </div>
-        <div class="new-post layui-hide-sm">
-          <a href="" class="layui-btn">发布新帖</a>
+        <div class="layui-col-md4">
+          <simu-tips></simu-tips>
+          <simu-sign></simu-sign>
+          <simu-hotlist></simu-hotlist>
+          <simu-ads></simu-ads>
+          <simu-links></simu-links>
         </div>
       </div>
     </div>
@@ -35,65 +19,26 @@
 </template>
 
 <script>
+import Pannel from '@/components/Pannel'
+import Tips from '@/components/sidebar/Tips'
+import Sign from '@/components/sidebar/Sign'
+import HotList from '@/components/sidebar/HotList'
+import Ads from '@/components/sidebar/Ads'
+import Links from '@/components/sidebar/Links'
+import List from '@/components/contents/List'
 export default {
-  name: 'home'
+  name: 'home',
+  components: {
+    'simu-pannel': Pannel,
+    'simu-tips': Tips,
+    'simu-sign': Sign,
+    'simu-hotlist': HotList,
+    'simu-ads': Ads,
+    'simu-links': Links,
+    'simu-list': List
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-@media screen and (max-width: 768px) {
-  .panel {
-    height: auto !important ;
-  }
-  ul {
-    li {
-      width: 33.333%;
-      text-align: center;
-    }
-  }
-}
-.panel {
-  position: relative;
-  height: 50px;
-  line-height: 50px;
-  background-color: #fff;
-  border-radius: 2px;
-  margin-bottom: 15px;
-  box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.05);
-  ul {
-    li {
-      display: inline-block;
-      a {
-        padding: 0 20px;
-      }
-    }
-    .line {
-      display: inline-block;
-      height: 15px;
-      width: 1px;
-      background-color: #e2e2e2;
-      vertical-align: middle;
-      margin: 0 20px;
-    }
-  }
-  .right {
-    position: absolute;
-    top: 0;
-    right: 0;
-    .layui-icon-search {
-      margin-right: 10px;
-      vertical-align: middle;
-      font-size: 20px;
-      &:hover {
-        color: #5fb878;
-        cursor: pointer;
-      }
-    }
-  }
-  .new-post {
-    text-align: center;
-    margin-top: 10px;
-    padding-bottom: 15px;
-  }
-}
 </style>
